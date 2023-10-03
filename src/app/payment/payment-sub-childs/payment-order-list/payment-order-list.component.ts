d3b92142f3d5ab7b1357efa36e3ff11eac86a21c
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-03 13:09:27 +0300

    Finishing work on payment-order-list component

--- src/app/payment/payment-sub-childs/payment-order-list/payment-order-list.component.ts
+++ src/app/payment/payment-sub-childs/payment-order-list/payment-order-list.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment-order-list.component.scss']
 })
 export class PaymentOrderListComponent {
+  totalPrice:number = 0;
 
+  orderList= [
+    {
+      id: 0,
+      name: "item 1",
+      price: 10,
+      quantity: 2
+    },
+    
+    {
+      id: 1,
+      name: "item 2",
+      price: 5,
+      quantity: 1
+    },
+
+    {
+      id: 2,
+      name: "item 3",
+      price: 10,
+      quantity: 2
+    },
+  ]
+
+  ngOnInit() {
+    this.totalPrice = this.orderList.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+  }
 }

b4e3fe1128a3d64bf6b3618573e599864836e724
siefhane <siefhane@gmail.com>
2023-10-10 21:26:08 +0300

    home-view

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    BannerComponent
+  ],
   imports: [
     CommonModule
   ]

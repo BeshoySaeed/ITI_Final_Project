0854903f6a8945f3de1ff79aa61f1d139b59401f
siefhane <siefhane@gmail.com>
2023-10-12 20:11:20 +0300

    Fix the Carousel autoplay

--- src/app/home/partner/partner.component.ts
+++ src/app/home/partner/partner.component.ts
@@ -9,7 +9,10 @@ import { Carousel } from 'primeng/carousel';
 export class PartnerComponent implements OnInit {
   items: any[] = [];
   responsiveOptions: any[] | undefined;
-
+  @ViewChild('carousel') carousel: any;
+  startAutoplay() {
+    this.carousel.startAutoplay();
+  }
   ngOnInit() {
     this.items = [
       {
@@ -54,22 +57,6 @@ export class PartnerComponent implements OnInit {
       }
     ];
 
-    this.responsiveOptions = [
-      {
-        breakpoint: '1400px',
-        numVisible: 3,
-        numScroll: 3
-      },
-      {
-        breakpoint: '1220px',
-        numVisible: 2,
-        numScroll: 2
-      },
-      {
-        breakpoint: '1100px',
-        numVisible: 1,
-        numScroll: 1
-      }
-    ];
+  
   }
 }

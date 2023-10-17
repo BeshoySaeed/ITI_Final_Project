800ffddfc2b38d2274772ed48f30b67b812909e5
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-18 01:13:57 +0300

    Show all categories - admin

--- src/app/admin/categories/categories/categories.component.ts
+++ src/app/admin/categories/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-categories',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent {
+  categories = [
+    {
+      id: 1,
+      name: "Category 1",
+      image: "https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg"
+    },
 
+    {
+      id: 2,
+      name: "Category 2",
+      image: "https://www.foodnetwork.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg"
+    },
+  ];
+
+  loading: boolean = false;
+
+  ngOnInit() {
+  }
+
+  clear(table: Table) {
+      table.clear();
+  }
+
+  applyFilterGlobal($event:any, dt:any, stringVal:string) {
+    dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  }
+
+  deleteUser(id:number) {
+    console.log(id)
+  }
 }

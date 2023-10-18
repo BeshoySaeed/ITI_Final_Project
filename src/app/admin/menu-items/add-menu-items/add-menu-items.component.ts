6e494d67085233346495b3061e43fc1492f29ba9
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-18 04:48:40 +0300

    Start working on add menu item - admin

--- src/app/admin/menu-items/add-menu-items/add-menu-items.component.ts
+++ src/app/admin/menu-items/add-menu-items/add-menu-items.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-menu-items',
   templateUrl: './add-menu-items.component.html',
-  styleUrls: ['./add-menu-items.component.scss']
+  styleUrls: ['./add-menu-items.component.scss'],
 })
 export class AddMenuItemsComponent {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: [''],
+      price: [''],
+      discount: [''],
+      active: [''],
+      description: [''],
+      category: [''],
+      image: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
+  }
+
+  onSelect(event: any) {
+    const file = event.files[0];
+    this.form.patchValue({
+      image: file,
+    });
+  }
+
+  onRemove() {
+    this.form.patchValue({
+      image: null,
+    });
+  }
 }

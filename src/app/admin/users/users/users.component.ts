78024f5863071c46940607f27d24db83a1114763
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-18 19:06:53 +0300

    Changes on users - admin

--- src/app/admin/users/users/users.component.ts
+++ src/app/admin/users/users/users.component.ts
@@ -4,89 +4,70 @@ import { Table } from 'primeng/table';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent {
   users = [
     {
       id: 1,
-      role: "User",
+      role: 'User',
       firstName: 'Martina',
-      lastName: "Magdi",
-      email: "martinamagdi@gmail.com",
-      subscriptionPlan: "Plan 1",
+      lastName: 'Magdi',
+      email: 'martinamagdi@gmail.com',
+      subscriptionPlan: 'Plan 1',
       balance: 1000,
       phones: [
         {
           id: 1,
-          value: '01254646441'
+          value: '01254646441',
         },
         {
           id: 2,
-          value: '01254655441'
+          value: '01254655441',
         },
       ],
-      addresses: [
-        {
-          id: 1,
-          value: 'Cairo'
-        },
-        {
-          id: 2,
-          value: 'Cairo'
-        },
-      ],
+      address: 'Cairo',
       totalOrders: 5,
-      totalPayed: 200
+      totalPayed: 200,
     },
 
     {
       id: 2,
-      role: "Admin",
+      role: 'Admin',
       firstName: 'Martina',
-      lastName: "Magdi",
-      email: "martinamagdi@gmail.com",
-      subscriptionPlan: "Plan 2",
+      lastName: 'Magdi',
+      email: 'martinamagdi@gmail.com',
+      subscriptionPlan: 'Plan 2',
       balance: 2000,
       phones: [
         {
           id: 1,
-          value: '01254646441'
+          value: '01254646441',
         },
         {
           id: 2,
-          value: '01254655441'
+          value: '01254655441',
         },
       ],
-      addresses: [
-        {
-          id: 1,
-          value: 'Cairo'
-        },
-        {
-          id: 2,
-          value: 'Cairo'
-        },
-      ],
+      address: 'Giza',
       totalOrders: 10,
-      totalPayed: 500
-    }
+      totalPayed: 500,
+    },
   ];
 
   loading: boolean = false;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   clear(table: Table) {
-      table.clear();
+    table.clear();
   }
 
-  applyFilterGlobal($event:any, dt:any, stringVal:string) {
+  applyFilterGlobal($event: any, dt: any, stringVal: string) {
     dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
   }
 
-  deleteUser(id:number) {
-    console.log(id)
+  deleteUser(id: number) {
+    console.log(id);
   }
 }

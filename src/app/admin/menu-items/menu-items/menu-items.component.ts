c11a589c2353bfe7a8bd8ad1f4288306243023be
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-18 03:14:01 +0300

    Start working on menu-items component - admin

--- src/app/admin/menu-items/menu-items/menu-items.component.ts
+++ src/app/admin/menu-items/menu-items/menu-items.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-menu-items',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-items.component.scss']
 })
 export class MenuItemsComponent {
+  menuItems = [
+    {
+      id: 1,
+      name: "Menu Item 1",
+      image: "https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg",
+      description: "This is the first item in our menu.",
+      price: 20,
+      category: "pizza",
+      active: true,
+      discount: 10
+    },
+  ];
 
+  loading: boolean = false;
+
+  ngOnInit() {
+  }
+
+  clear(table: Table) {
+      table.clear();
+  }
+
+  applyFilterGlobal($event:any, dt:any, stringVal:string) {
+    dt.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  }
+
+  deleteUser(id:number) {
+    console.log(id)
+  }
 }

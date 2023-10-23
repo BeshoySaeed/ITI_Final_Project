07b5b2b6031e4492cf8f878a969eb1c2d073ba27
Gerges Medhat <gergesmedhat414@gmail.com>
2023-10-23 23:41:42 +0100

    user-services

--- src/app/admin/users/users/users.component.ts
+++ src/app/admin/users/users/users.component.ts
@@ -61,19 +61,7 @@ export class UsersComponent {
 
 
 
- private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
-  }
+
 
 
 

25c29ffb87f7302f3a2f23d847705094b214ea22
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-24 01:26:49 +0300

    phones & emails services

--- src/app/services/Customer service data/emails/customer-service-emails.service.ts
+++ src/app/services/Customer service data/emails/customer-service-emails.service.ts
@@ -1,37 +1,71 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, retry } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
+import { throwError } from 'rxjs/internal/observable/throwError';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerServiceEmailsService {
-  emailsApiRoute = 'customer-service-emails'
+  emailsApiRoute = 'customer-service-emails';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getAllEmails(): Observable<object> {
-    return this.httpClient.get<object>(`${environment.host}${this.emailsApiRoute}`)
+    return this.httpClient.get<object>(`${environment.host}${this.emailsApiRoute}`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   getAllActiveEmails(): Observable<object> {
-    return this.httpClient.get<object>(`${environment.host}customer-service-active-emails`)
+    return this.httpClient.get<object>(`${environment.host}customer-service-active-emails`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   getEmailById(id: number): Observable<object> {
-    return this.httpClient.get<object>(`${environment.host}${this.emailsApiRoute}/${id}`)
+    return this.httpClient.get<object>(`${environment.host}${this.emailsApiRoute}/${id}`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   deleteEmailById(id: number): Observable<object> {
-    return this.httpClient.delete<object>(`${environment.host}${this.emailsApiRoute}/${id}`)
+    return this.httpClient.delete<object>(`${environment.host}${this.emailsApiRoute}/${id}`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   updateEmail(id:number, email: object): Observable<object> {
-    return this.httpClient.put<object>(`${environment.host}${this.emailsApiRoute}/${id}`, email)
+    return this.httpClient.put<object>(`${environment.host}${this.emailsApiRoute}/${id}`, email).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   storeEmail(email: object): Observable<object> {
-    return this.httpClient.post<object>(`${environment.host}${this.emailsApiRoute}`, email)
+    return this.httpClient.post<object>(`${environment.host}${this.emailsApiRoute}`, email).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
--- src/app/services/Customer service data/phones/customer-service-phones.service.ts
+++ src/app/services/Customer service data/phones/customer-service-phones.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, retry, throwError } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment.development';
 
@@ -12,26 +13,58 @@ export class CustomerServicePhonesService {
   constructor(private httpClient: HttpClient) { }
 
   getAllPhones(): Observable<object> {
-    return this.httpClient.get<object>(`${environment.host}${this.phonesApiRoute}`)
+    return this.httpClient.get<object>(`${environment.host}${this.phonesApiRoute}`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   getAllActivePhones(): Observable<object> {
-    return this.httpClient.get<object>(`${environment.host}customer-service-active-phones`)
+    return this.httpClient.get<object>(`${environment.host}customer-service-active-phones`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   getPhoneById(id: number): Observable<object> {
-    return this.httpClient.get<object>(`${environment.host}${this.phonesApiRoute}/${id}`)
+    return this.httpClient.get<object>(`${environment.host}${this.phonesApiRoute}/${id}`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   deletePhoneById(id: number): Observable<object> {
-    return this.httpClient.delete<object>(`${environment.host}${this.phonesApiRoute}/${id}`)
+    return this.httpClient.delete<object>(`${environment.host}${this.phonesApiRoute}/${id}`).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   updatePhone(id:number, phone: object): Observable<object> {
-    return this.httpClient.put<object>(`${environment.host}${this.phonesApiRoute}/${id}`, phone)
+    return this.httpClient.put<object>(`${environment.host}${this.phonesApiRoute}/${id}`, phone).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
   storePhone(phone: object): Observable<object> {
-    return this.httpClient.post<object>(`${environment.host}${this.phonesApiRoute}`, phone)
+    return this.httpClient.post<object>(`${environment.host}${this.phonesApiRoute}`, phone).pipe(
+      retry(3), // retry a failed request up to 3 times
+      catchError(this.handleError) // then handle the error
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }

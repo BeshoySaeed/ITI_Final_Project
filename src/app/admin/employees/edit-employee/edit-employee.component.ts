17624dd0a98678990337fd734e42ac3b35ddbd50
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-25 19:25:15 +0300

    Edit subscribe plan using API

--- src/app/admin/employees/edit-employee/edit-employee.component.ts
+++ src/app/admin/employees/edit-employee/edit-employee.component.ts
@@ -44,7 +44,6 @@ export class EditEmployeeComponent {
   ngOnInit() {
     this.initializeForm();
     this.getAllBranches();
-    console.log(this.branches)
 
     this.getEmployee()
       .then((response) => {
--- src/app/admin/subscribe/edit-subscribe/edit-subscribe.component.ts
+++ src/app/admin/subscribe/edit-subscribe/edit-subscribe.component.ts
@@ -1,41 +1,114 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { SubscriptionsService } from 'src/app/services/Subscriptions/subscriptions.service';
 
 @Component({
   selector: 'app-edit-subscribe',
   templateUrl: './edit-subscribe.component.html',
-  styleUrls: ['./edit-subscribe.component.scss']
+  styleUrls: ['./edit-subscribe.component.scss'],
+  providers: [MessageService],
 })
 export class EditSubscribeComponent {
-  isContacted: boolean = false;
+  subscribeId = this.activeRoute.snapshot.params['id'];
+  loader = true;
   EditSubscribe!: FormGroup;
-  constructor(private fb: FormBuilder) {}
+  editorColors = [
+    'black',
+    'white',
+    'red',
+    'blue',
+    'green',
+    '#fd702a',
+    'yellow',
+  ];
+  formControllers = [
+    'name',
+    'benefit',
+    'discount_value',
+    'subscribe_value',
+    'duration',
+    'subscribe_value',
+  ];
+  subscribe: any = {};
+
+  constructor(
+    private fb: FormBuilder,
+    private activeRoute: ActivatedRoute,
+    private subscriptionsService: SubscriptionsService,
+    private messageService: MessageService
+  ) {}
+
   ngOnInit() {
-    this.EditSubscribe = this.fb.group(
-      {
-        name: ['', Validators.pattern(/^[a-zA-Z]+$/)],
-        benefits:['', [Validators.pattern(/^[0-9]+$/), Validators.required]],
-        discount_value:['', [Validators.pattern(/^[0-9]+$/), Validators.required]],
-        duration:['', [Validators.pattern(/^[0-9]+$/), Validators.required]],
-        subscribe_value:['', [Validators.pattern(/^[0-9]+$/), Validators.required]],
-       
-      },
+    this.initializeForm();
+
+    this.getSubscribe()
+      .then((response) => {
+        this.subscribe = response.data;
+      })
+      .then(() => {
+        this.setFormValues();
+        this.loader = false;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  initializeForm() {
+    this.EditSubscribe = this.fb.group({
+      name: [''],
+      benefit: [''],
+      discount_value: [
+        '',
+        [Validators.pattern(/^[0-9]+$/), Validators.required],
+      ],
+      duration: ['', [Validators.pattern(/^[0-9]+$/), Validators.required]],
+      subscribe_value: [
+        '',
+        [Validators.pattern(/^[0-9]+$/), Validators.required],
+      ],
+      active: [true],
+    });
+  }
+
+  setFormValues() {
+    for (let control of this.formControllers) {
+      this.EditSubscribe.controls[control].setValue(this.subscribe[control]);
+    }
+    this.EditSubscribe.controls['active'].setValue(
+      this.subscribe.active == 1 ? true : false
     );
   }
-  toggleContacted(feedback: any) {
-    this.isContacted = feedback;
 
-    if (feedback) {
-      console.log(feedback.id);
-    } else {
-      console.log(feedback.id);
-    }
+  getSubscribe(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.subscriptionsService.getById(this.subscribeId).subscribe(
+        (response) => {
+          resolve(response);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
   }
-  getToggleButtonClass() {
-    return this.isContacted ? 'contacted' : 'not-contacted';
-  }
+
   onSubmit() {
-    console.log(this.EditSubscribe);
-  }
+    this.loader = true;
 
+    this.subscriptionsService
+      .update(this.subscribeId, this.EditSubscribe.value)
+      .subscribe((response: any) => {
+        if (response.status == 'success') {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Subscribe plan is updated',
+          });
+          this.loader = false;
+        }
+      });
+  }
 }

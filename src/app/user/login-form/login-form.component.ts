c6e7e4ca898bb4cedc860b0b27f6d4691bc15c1a
siefhane <siefhane@gmail.com>
2023-10-28 16:05:55 +0200

    Auth

--- src/app/user/login-form/login-form.component.ts
+++ src/app/user/login-form/login-form.component.ts
@@ -1,5 +1,9 @@
+import { formatDate } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/Auth/auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -9,16 +13,37 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginFormComponent {
   value : any ;
   formGroup : FormGroup;
-  constructor(){
+  constructor(private http: HttpClient, private router: Router ,private login: AuthService){
     this.formGroup = new FormGroup({
-      emailControl : new FormControl('',[Validators.required,
-        Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]),
-      passControl : new FormControl('', [
-        Validators.required,
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@%$#])[a-zA-Z\\d@%$#]+$')
+      email : new FormControl('',[Validators.required,
+        // Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]
       ]),
-        checkControl : new FormControl('',[Validators.required])
+      password : new FormControl('', [
+        Validators.required,
+        // Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@%$#])[a-zA-Z\\d@%$#]+$')
+      ]),
+
 
     })
   }
+  onSubmit() {
+    const apiUrl = 'http://localhost:8000/api/login'; // Replace with your actual API URL
+    const formData=this.formGroup.getRawValue();
+    const data={
+      email: formData.email,
+      password: formData.password,
+    }
+    console.log(data)
+    this.http.post(apiUrl, formData).subscribe(
+      (response: any) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/home']); // Redirect to the dashboard page
+      },
+      (error) => {
+        console.error('Login failed:', error);
+      }
+    );
+  }
+
+
 }

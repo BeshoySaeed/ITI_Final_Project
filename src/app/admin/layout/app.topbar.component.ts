c4970b3512fe1d57c38c7a1f3ec03f0f1d7b0798
MartinaMagdi <martinmagdi6@gmail.com>
2023-10-29 19:31:23 +0200

    Admin logout using api

--- src/app/admin/layout/app.topbar.component.ts
+++ src/app/admin/layout/app.topbar.component.ts
@@ -1,21 +1,35 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { LayoutService } from "./service/app.layout.service";
+import { LayoutService } from './service/app.layout.service';
+import { AuthService } from 'src/app/services/Auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+  selector: 'app-topbar',
+  templateUrl: './app.topbar.component.html',
 })
 export class AppTopBarComponent {
-    id:number = 2
+  id: number = 2;
 
-    items!: MenuItem[];
+  items!: MenuItem[];
 
-    @ViewChild('menubutton') menuButton!: ElementRef;
+  @ViewChild('menubutton') menuButton!: ElementRef;
 
-    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
+  @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
 
-    @ViewChild('topbarmenu') menu!: ElementRef;
+  @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+  constructor(public layoutService: LayoutService, private authService: AuthService, private router: Router) {}
+
+  logout() {
+    const data = {
+      token: localStorage.getItem('token'),
+    };
+
+    this.authService.logout(data).subscribe((response: any) => {});
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('role_id');
+    this.router.navigate(['/home']);
+  }
 }

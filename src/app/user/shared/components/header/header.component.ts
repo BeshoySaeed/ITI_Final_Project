b025c027e0e574a7a2c9d0a06209731dc168a64a
Gerges Medhat <gergesmedhat414@gmail.com>
2023-10-29 12:20:11 +0000

    logout in front & back

--- src/app/user/shared/components/header/header.component.ts
+++ src/app/user/shared/components/header/header.component.ts
@@ -6,28 +6,50 @@ import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user-service/user.service';
 import { AuthService } from 'src/app/services/Auth/auth.service';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  providers: [MessageService],
 })
 export class HeaderComponent implements OnInit{
   authService = inject(AuthService);
   isLoggedIn:boolean = false;
 
   userName: string = "";
-  
+
+  constructor(private messageService: MessageService, private router: Router) {}  
+
+
 ngOnInit(): void {
   this.authService.isLoggedIn$.subscribe(res=>{
    this.isLoggedIn = this.authService.isLoggedIn();
 
   })
 }
-
+/*
   logout(){
     localStorage.removeItem("token");
     this.authService.isLoggedIn$.next(false);
 
+  }
+*/
+  logout(user: any) {
+    this.authService
+      .logout(user)
+      .subscribe((response: any) => {
+        if (response.status == 'success') {
+          localStorage.removeItem("token");
+          this.authService.isLoggedIn$.next(false);
+          this.router.navigate(['/home']); // Redirect to the dashboard page
+
+          this.messageService.add({
+
+            detail: 'user logged out',
+          });
+        }
+      });
   }
 }
   

b066f572442b11d78e5eda9d0a3512afd4656147
Gerges Medhat <gergesmedhat414@gmail.com>
2023-11-03 20:18:01 +0000

    pipe

--- src/app/pipe/subscription.pipe.ts
+++ src/app/pipe/subscription.pipe.ts
@@ -1,8 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { SubscriptionsService } from '../services/Subscriptions/subscriptions.service'; 
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
 
 @Pipe({
   name: 'subscription'
@@ -22,33 +19,25 @@ export class SubscriptionPipe implements PipeTransform {
   
   transform(value: any): any {
     this.subId= localStorage.getItem('subscribe_id');
+    if (this.subId!=null) {
+      this.getSubByID();
+       this.percentage = localStorage.getItem('discount_value');   
 
-    if (this.subId != null) {
-      return this.getSubByID().pipe(
-        map(() => {
-          if (typeof value === 'string') {
-            if (this.percentage) {
-               this.discount = Number(value) * (this.percentage / 100);
-              return Number(value) - this.discount;
-            }
-          }
-          return value;
-        })
-      );
-    } else {
-      return value;
+      this.discount= value * (this.percentage / 100);
+      return value - this.discount; 
+    
+    }
+     else {
+
+      return value ;
     }
   }
 
- 
-  getSubByID(): Observable<any> {
-    return this.httpSub.getById(this.subId).pipe(
-      map((subData: any) => {
-        this.subscribeObject = subData.data;
-        this.percentage = this.subscribeObject.discount_value;
-        return subData; 
-      })
-    );
+  getSubByID() : any{
+    this.httpSub.getById(this.subId).subscribe((subData: any) => {
+      this.subscribeObject = subData.data;
+      localStorage.setItem("discount_value", this.subscribeObject.discount_value);  
+    })
   }
 
 }

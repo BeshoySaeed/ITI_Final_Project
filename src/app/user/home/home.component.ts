458cbe645a4aa73f8cee64848690e863e7ae8143
siefhane <siefhane@gmail.com>
2023-11-03 18:40:54 +0200

    Fix issue

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,inject } from '@angular/core';
+
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/Auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,18 +11,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   response: any;
+  authService = inject(AuthService);
 
-  constructor(private http: HttpClient) {}
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.http.get<any>('http://127.0.0.1:8000/api/response-data').subscribe(
-      (response: any) => {
-        this.response = response;
-        console.log(this.response); // Access the response data as needed
-      },
-      (error) => {
-        console.error('Failed to retrieve response data:', error);
-      }
+    this.route.queryParams
+    .subscribe(params => {
+      const message = params['original[message]'];
+      const status = params['original[status]'];
+      const token = params['original[token]'];
+      const userId = params['original[user_id]'];
+      const role_id = params['original[role_id]'];
+      console.log(message);
+      if (role_id == 2) {
+              localStorage.setItem('role_id', role_id);
+              localStorage.setItem('user_id', userId);
+          }
+            if (status=='success') {
+              localStorage.setItem('token', token);
+              this.authService.isLoggedIn$.next(true);
+            }
+            else{
+              console.error(params);
+      
+            }
+      
+    }
     );
   }
 }

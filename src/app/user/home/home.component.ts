aea87ecb1edeeb80ab153584b69173e505f01928
siefhane <siefhane@gmail.com>
2023-11-03 18:18:06 +0200

    Google  & Payment

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-import { BannerComponent } from './banner/banner.component';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  response: any;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://127.0.0.1:8000/api/response-data').subscribe(
+      (response: any) => {
+        this.response = response;
+        console.log(this.response); // Access the response data as needed
+      },
+      (error) => {
+        console.error('Failed to retrieve response data:', error);
+      }
+    );
+  }
 }

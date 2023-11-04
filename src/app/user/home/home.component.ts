7b51785fd6a815f1a14888e38468704fb049f793
Gerges Medhat <gergesmedhat414@gmail.com>
2023-11-04 20:28:07 +0000

    user subscribe

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -8,28 +8,5 @@ import { BannerComponent } from './banner/banner.component';
 })
 export class HomeComponent {
 
-  checkDate: any;
-  todayDate:any;
-
-  ngOnInit(){
-    this.checkEndDate();  //check if subscription is ended or not 
-  }
-
-  checkEndDate()
-  {
-    if(localStorage.getItem('subscribe_id')){
-      console.log('check date')
-      this.todayDate =new Date();
-      this.checkDate = localStorage.getItem('endDate');
-      if(this.todayDate>=this.checkDate){
-
-      localStorage.removeItem('subscribe_id');
-      localStorage.removeItem('startDate');
-      localStorage.removeItem('endDate');
-      localStorage.removeItem('discount_value');
-
-      }
-    }
-    
-  }
+  
 }

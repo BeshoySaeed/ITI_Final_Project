b976038ef9a34314d1b84ac61784d5d633aaadb5
MartinaMagdi <martinmagdi6@gmail.com>
2023-11-07 21:15:55 +0200

    New password regex

--- src/app/user/new-password/new-password.component.ts
+++ src/app/user/new-password/new-password.component.ts
@@ -20,7 +20,7 @@ export class NewPasswordComponent {
       password: ['', [
         Validators.required,
         Validators.minLength(8),
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@*%$#])[a-zA-Z\\d@*%$#]+$')
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
         
       ]],    
       token: ['', [

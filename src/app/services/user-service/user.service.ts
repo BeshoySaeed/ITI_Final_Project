bf796f9cf92a693ca3b8b6ae2ed2d0c8998561fd
BeshoySaeed <said.beshoy@yahoo.com>
2023-11-12 11:45:12 -0500

    update user-balance

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -1,63 +1,82 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment.development';
 import { catchError, retry, throwError } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   headers = new HttpHeaders({
-    'Authorization': `Bearer ${localStorage.getItem('token')}`
-  })
-  
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  });
+
   constructor(private httpClient: HttpClient) {}
 
   getAllUsers(): Observable<object> {
-    return this.httpClient.get<object>(`${environment.host}/users`, {headers: this.headers}).pipe(
-      retry(3), // retry a failed request up to 3 times
-      catchError(this.handleError) // then handle the error
-    );
+    return this.httpClient
+      .get<object>(`${environment.host}/users`, { headers: this.headers })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
   }
 
-
-  getUserByID(id: any): Observable<any>{
-    return this.httpClient.get<object>(`${environment.host}/users/${id}`, {headers: this.headers}).pipe(
-      retry(3), // retry a failed request up to 3 times
-      catchError(this.handleError) // then handle the error
-    );
+  getUserByID(id: any): Observable<any> {
+    return this.httpClient
+      .get<object>(`${environment.host}/users/${id}`, { headers: this.headers })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
   }
 
-  
   storeUser(user: object): Observable<object> {
-    return this.httpClient.post<object>(`${environment.host}/users`, user, {headers: this.headers}).pipe(
-      retry(3), // retry a failed request up to 3 times
-      catchError(this.handleError) // then handle the error
-    );
+    return this.httpClient
+      .post<object>(`${environment.host}/users`, user, {
+        headers: this.headers,
+      })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
   }
-  setSubIdValue(id:any ,subscriptionData: object): Observable<object> {
-    return this.httpClient.put<object>(`${environment.host}/subs/${id}/subID`, subscriptionData ,{headers: this.headers}).pipe(
-      retry(3), // retry a failed request up to 3 times
-      catchError(this.handleError) // then handle the error
-    ); 
+  setSubIdValue(id: any, subscriptionData: object): Observable<object> {
+    return this.httpClient
+      .put<object>(`${environment.host}/subs/${id}/subID`, subscriptionData, {
+        headers: this.headers,
+      })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
   }
 
-  
-  updateUser(id:number, user: object): Observable<object> {
-    return this.httpClient.patch<object>(`${environment.host}/users/${id}`, user, {headers: this.headers}).pipe(
-      retry(3), // retry a failed request up to 3 times
-      catchError(this.handleError) // then handle the error
-    );
+  updateUser(id: any, user: object): Observable<object> {
+    return this.httpClient
+      .patch<object>(`${environment.host}/users/${id}`, user, {
+        headers: this.headers,
+      })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
   }
-  
 
   deleteUserById(id: number): Observable<object> {
-    return this.httpClient.delete<object>(`${environment.host}/users/${id}`, {headers: this.headers}).pipe(
-      retry(3), // retry a failed request up to 3 times
-      catchError(this.handleError) // then handle the error
-    );
+    return this.httpClient
+      .delete<object>(`${environment.host}/users/${id}`, {
+        headers: this.headers,
+      })
+      .pipe(
+        retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
   }
 
   private handleError(error: HttpErrorResponse) {
@@ -68,10 +87,13 @@ export class UserService {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
     }
     // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
   }
-
 }
--- src/app/user/home/subscription-section/subscription-section.component.ts
+++ src/app/user/home/subscription-section/subscription-section.component.ts
@@ -5,49 +5,47 @@ import { SubscriptionPlan } from 'src/app/interface/subscription-plan';
 import { UserService } from 'src/app/services/user-service/user.service';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-subscription-section',
   templateUrl: './subscription-section.component.html',
   styleUrls: ['./subscription-section.component.scss'],
 })
 export class SubscriptionSectionComponent {
- 
-
   constructor(
     private subscriptionsService: SubscriptionsService,
-    private userService:UserService,
+    private userService: UserService,
     private sanitizer: DomSanitizer,
-    private http: HttpClient
+    private http: HttpClient,
+    private httpUser: UserService
   ) {}
 
   subscriptions: SubscriptionPlan[] = [];
-  subId:any;
-  currentDate : any ;
+  subId: any;
+  currentDate: any;
   endDate: any;
-  duration:any;
-  userId=localStorage.getItem('user_id');
-  user: any = []
-  check:any = null;
+  duration: any;
+  userId = localStorage.getItem('user_id');
+  user: any = [];
+  check: any = null;
   year: any;
   month: any;
   day: any;
-  subs_price:any;
-  formattedCurrentDate:any;
-  formattedEndDate: any; 
+  subs_price: any;
+  formattedCurrentDate: any;
+  formattedEndDate: any;
   checkDate: any;
-  todayDate:any;
+  todayDate: any;
 
-  subscriptionData: any ={
-    subscribe_id : null,
-    start_date : null,
-    end_date: null
-};
+  subscriptionData: any = {
+    subscribe_id: null,
+    start_date: null,
+    end_date: null,
+  };
 
   ngOnInit() {
     this.getAll();
     this.checkSub();
-    this.checkEndSub()
+    this.checkEndSub();
   }
 
   getAll() {
@@ -60,75 +58,75 @@ export class SubscriptionSectionComponent {
     return this.sanitizer.bypassSecurityTrustHtml(value);
   }
 
-  
-   SubscribeButton(subId:any) {
+  SubscribeButton(subId: any) {
     this.subscriptionsService.getById(subId).subscribe((subData: any) => {
       this.duration = subData.data.duration;
       this.currentDate = new Date();
       this.endDate = new Date(this.currentDate);
-      this.endDate.setDate(this.currentDate.getDate() + (this.duration*30));
+      this.endDate.setDate(this.currentDate.getDate() + this.duration * 30);
       this.formattedCurrentDate = this.formatDate(this.currentDate);
       this.formattedEndDate = this.formatDate(this.endDate);
-      this.subs_price=subData.data.subscribe_value;
-      this.subscriptionData={ 
-       subscribe_id : subId,
-       start_date :  this.formattedCurrentDate,
-       end_date : this.formattedEndDate     
-     };
-     const value = this.subs_price; 
-      console.log("value",value)
+      this.subs_price = subData.data.subscribe_value;
+      this.subscriptionData = {
+        subscribe_id: subId,
+        start_date: this.formattedCurrentDate,
+        end_date: this.formattedEndDate,
+      };
+      const value = this.subs_price;
+      console.log('value', value);
       const body = { value };
-      this.http.post<any>('http://localhost:8000/api/paypal/paymentsubs', body).subscribe(
-      response => {
-        console.log(response)
-        if(response.success){
-          this.userService.setSubIdValue(this.userId, this.subscriptionData).subscribe();
-          const url=response.link;
-          window.location.href=url;
-        }
-      },
-      error => {
-        console.error('Failed to initiate payment:', error);
-      }
-    );
-     
-    })
+      this.http
+        .post<any>('http://localhost:8000/api/paypal/paymentsubs', body)
+        .subscribe(
+          (response) => {
+            console.log(response);
+            if (response.success) {
+              this.userService
+                .setSubIdValue(this.userId, this.subscriptionData)
+                .subscribe();
+              const url = response.link;
+              window.location.href = url;
+            }
+          },
+          (error) => {
+            console.error('Failed to initiate payment:', error);
+          }
+        );
 
+      this.httpUser
+        .updateUser(this.userId, {
+          balance: subData.data.subscribe_value,
+        })
+        .subscribe();
+    });
   }
-  
-  checkSub(){
- 
+
+  checkSub() {
     this.userService.getUserByID(this.userId).subscribe((response: any) => {
       this.check = response.data.subscribe_id;
-      console.log(response)
+      console.log(response);
     });
-    
   }
 
   formatDate(date: Date): string {
     this.year = date.getFullYear();
-   this.month = String(date.getMonth() + 1).padStart(2, '0');
-   this.day = String(date.getDate()).padStart(2, '0');
-   return `${this.year}-${this.month}-${this.day}`;
- }
+    this.month = String(date.getMonth() + 1).padStart(2, '0');
+    this.day = String(date.getDate()).padStart(2, '0');
+    return `${this.year}-${this.month}-${this.day}`;
+  }
 
-  checkEndSub()
-  {
-    
-    this.userService.getUserByID(this.userId).subscribe((response: any)=> {
+  checkEndSub() {
+    this.userService.getUserByID(this.userId).subscribe((response: any) => {
       this.subId = response.data.subscribe_id;
       this.checkDate = response.data.end_date;
-      if(this.subId){
-        this.todayDate =new Date();
-        if(this.todayDate>=this.checkDate){
-          
-  
-          this.userService.setSubIdValue(this.userId, this.subscriptionData).subscribe();
-  
-    
-          }
+      if (this.subId) {
+        this.todayDate = new Date();
+        if (this.todayDate >= this.checkDate) {
+          this.userService
+            .setSubIdValue(this.userId, this.subscriptionData)
+            .subscribe();
+        }
       }
     });
-
   }
 }
